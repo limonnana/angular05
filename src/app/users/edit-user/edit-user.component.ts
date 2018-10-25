cd7da2e6be9b37471294a3549c87ba797aaf61c7
limonnana <rosenzvaig@gmail.com>
2018-10-25 13:26:03 +0300

    colorbutton

--- src/app/users/edit-user/edit-user.component.ts
+++ src/app/users/edit-user/edit-user.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from "@angular/router";
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { User } from '../../entities/user';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-user',
@@ -14,8 +14,8 @@ export class EditUserComponent implements OnInit {
   userId$: string;
   theUser: User;
   editForm: FormGroup;
-  id:string = '';
- 
+  id: string;
+
 
   constructor(private formBuilder: FormBuilder,private router: Router, private route: ActivatedRoute, private userService: UserService) { 
     this.route.params.subscribe( params => this.userId$ = params.id );
@@ -33,14 +33,14 @@ export class EditUserComponent implements OnInit {
       .subscribe( data => {
         this.id = data.id;
         this.editForm.setValue({
-        id:data.id,
-        name:data.name,
-        lastName:data.lastName,
-        email:data.email
+        id: data.id,
+        name: data.name,
+        lastName: data.lastName,
+        email: data.email
        });
       });
   
-    //this.userService.getUserById(this.userId$)
+    // this.userService.getUserById(this.userId$)
      // .subscribe( data => {
       //  this.theUser = data;
      // });
@@ -49,15 +49,15 @@ export class EditUserComponent implements OnInit {
   get f() { return this.editForm.controls; }
 
   onSubmit() {
-    //if (this.loginForm.valid) {
-      console.log("Form Values: " + this.f.name.value + " " + this.f.lastName.value + " " +  this.f.email.value);
+    // if (this.loginForm.valid) {
+      console.log('Form Values: ' + this.f.name.value + ' ' + this.f.lastName.value + ' ' +  this.f.email.value);
       this.userService.updateUser(this.editForm.value)
       .subscribe((response:any) => {console.log(response.response)})
       this.router.navigate(['users']);
 }
 
-  changePassword(userId : string){
-    this.router.navigate(['changePassword/'+ this.userId$]);
+  changePassword(userId: string){
+    this.router.navigate(['changePassword/' + this.userId$]);
   }
 
 }
